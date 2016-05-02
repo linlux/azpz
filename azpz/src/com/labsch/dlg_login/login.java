625a77b08d900a2180798ceec92a13063704368d
Putcho44 <luethkematthias@gmail.com>
2016-05-02 10:13:49 +0200

    Login Dialog geändert

--- azpz/src/com/labsch/dlg_login/login.java
+++ azpz/src/com/labsch/dlg_login/login.java
@@ -1,6 +1,12 @@
 package com.labsch.dlg_login;
 
-import java.util.Date;
+import java.sql.ResultSet;
+
+import javax.swing.JOptionPane;
+
+import com.labsch.dbUtils.DBConnection;
+
+
 
 public class login
 
@@ -26,32 +32,32 @@ public class login
     }
 
     /**
-	 * @using	 * 
-	 *   . authenticate('ML' username, String 'geheim')
-	 * @param username
-	 *  Username
-	 * @param password
-	 * Passwort 	
-	 * @returnvalue
-	 *    true = User ist authentifiziert
-	 *    false = User ist NICHT authentifiziert oder Fehler
-	 */
+     * @using    * 
+     *   . authenticate('ML' username, String 'geheim')
+     * @param username
+     *  Username
+     * @param password
+     * Passwort     
+     * @returnvalue
+     *    true = User ist authentifiziert
+     *    false = User ist NICHT authentifiziert oder Fehler
+     */
     public  boolean authenticate(String username, String password )
     {
-	boolean breturn = true;
-	boolean bOK = false;
-	if ( !username.isEmpty() &&  !password.isEmpty()) 
-	    breturn = false;
-	
-	    bOK =  readUser(username) ;
-	    if (bOK)
-	    {
-	        if ( password.equals(dbUserPW) && username.equals(dbUserName))
-	            breturn = true;
-	    }
-	    
-    	 System.out.println(" authenticate =  " + username + ' ' + password + ' ' + breturn);
-    	 return breturn;
+    boolean breturn = true;
+    boolean bOK = false;
+    if ( !username.isEmpty() &&  !password.isEmpty()) 
+        breturn = false;
+    
+        bOK =  readUser(username) ;
+        if (bOK)
+        {
+            if ( password.equals(dbUserPW) && username.equals(dbUserName))
+                breturn = true;
+        }
+        
+         System.out.println(" authenticate =  " + username + ' ' + password + ' ' + breturn);
+         return breturn;
     }
     
     
@@ -92,126 +98,4 @@ public class login
     
     
     
-}ert
-	 *    false = User ist NICHT authentifiziert oder Fehler
-	 */
-    public  boolean authenticate(String username, String password )
-    {
-	boolean breturn = true;
-	boolean bOK = false;
-	if ( !username.isEmpty() &&  !password.isEmpty()) 
-	    breturn = false;
-	
-	    bOK =  readUser(username) ;
-	    if (bOK)
-	    {
-	        if ( password.equals(dbUserPW) && username.equals(dbUserName))
-	            breturn = true;
-	    }
-	    
-    	 System.out.println(" authenticate =  " + username + ' ' + password + ' ' + breturn);
-    	 return breturn;
-    }
-    
-    
-    
-    private   boolean readUser(String pUserName)
-    {
-        Boolean bOK =  false ;
-
-        String SQL = "SELECT   `user_id`,  `user_name`,  `pw_clear`  FROM   `user`";
-               SQL +=  "WHERE user_name =  "  + (pUserName.trim());        
-
-        ResultSet rSet = DBConnection.executeQuery(SQL);
-        if (rSet == null)
-            return bOK;
-
-        try
-        {
-            if (rSet.next())
-            {
-                dbUserName = rSet.getString("user_name");
-                dbUser_ID = rSet.getLong("user_id");
-                dbUserPW = rSet.getString("pw_clear");                               
-            }
-            else
-            {
-                JOptionPane.showMessageDialog(null, "Der Datensatz konnte nicht gefunden werden");
-                dbUser_ID = (long) -1;
-            }
-
-            rSet.close();
-
-        }
-        catch (Exception ex)
-        {
-        }
-        return bOK;        
-    }
-    
-    
-    
-}ert
-	 *    false = User ist NICHT authentifiziert oder Fehler
-	 */
-    public  boolean authenticate(String username, String password )
-    {
-	boolean breturn = true;
-	boolean bOK = false;
-	if ( !username.isEmpty() &&  !password.isEmpty()) 
-	    breturn = false;
-	
-	    bOK =  readUser(username) ;
-	    if (bOK)
-	    {
-	        if ( password.equals(dbUserPW) && username.equals(dbUserName))
-	            breturn = true;
-	    }
-	    
-    	 System.out.println(" authenticate =  " + username + ' ' + password + ' ' + breturn);
-    	 return breturn;
-    }
-    
-    
-    
-    private   boolean readUser(String pUserName)
-    {
-        Boolean bOK =  false ;
-
-        String SQL = "SELECT   `user_id`,  `user_name`,  `pw_clear`  FROM   `user`";
-               SQL +=  "WHERE user_name =  "  + (pUserName.trim());        
-
-        ResultSet rSet = DBConnection.executeQuery(SQL);
-        if (rSet == null)
-            return bOK;
-
-        try
-        {
-            if (rSet.next())
-            {
-                dbUserName = rSet.getString("user_name");
-                dbUser_ID = rSet.getLong("user_id");
-                dbUserPW = rSet.getString("pw_clear");                               
-            }
-            else
-            {
-                JOptionPane.showMessageDialog(null, "Der Datensatz konnte nicht gefunden werden");
-                dbUser_ID = (long) -1;
-            }
-
-            rSet.close();
-
-        }
-        catch (Exception ex)
-        {
-        }
-        return bOK;        
-    }
-    
-    
-    
-}+ ' ' + breturn);
-    	 return breturn;
-    }
-
 }
--- azpz/src/com/labsch/dlg_login/loginDialog.java
+++ azpz/src/com/labsch/dlg_login/loginDialog.java
@@ -1,97 +1,112 @@
 package com.labsch.dlg_login;
+
 /**
  * 
  * @author Matthias Lüthke,   27.04.2016
  *
  */
 import java.awt.*;
-import java.awt.Window.Type;
+
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.*;
 
+
 @SuppressWarnings("serial")
-public class loginDialog extends JDialog  implements  ActionListener
-{ 
-   // private JFrame frameLogin;
-    private static final boolean debug = true;  
+public class loginDialog extends JDialog implements ActionListener, WindowListener
+{
+    private final boolean debug = true;
     private JTextField tfUsername;
     private JPasswordField pfPassword;
     private JLabel lbUsername;
     private JLabel lbPassword;
     private JButton btnLogin;
     private JButton btnCancel;
-    private boolean succeeded;
-    private Frame parentFrame;     
+    private boolean succeeded = false;
+    private Frame parentFrame;
     
-    // Grösse der Componenten festlegen
+    private String dbUserName ;
+    private String dbUserPW ;
+    private long  dbUser_ID = (long)-1 ;
+    private Boolean bLogin = false;
     
-    private int  dlg_width= 325;
-    private int  dlg_height = 210;
-    private int  btn_width= 80;
-    private int  btn_height = 20;
-    private int  lbl_width= 60;
-    private int  lbl_height = 20;
-    private int  tf_width= 60;
-    private int  tf_height = 20;   
-        
+    private  login mlog;
+    
        
-    private void initDialog ()
-	{
-	        
-	        this.setModal(true);
-		//this.setLocationRelativeTo(owner); 	
+    private void initDialog()
+    {
+        this.setModal(true);
+        // this.setLocationRelativeTo(owner);
+    }
 
-	}
-       private void initializeComponents(Frame parent)
-	{
-	   
-	 parentFrame = parent;
-        
-        
-        this.setSize(dlg_width, dlg_height);
-        this.setLocationRelativeTo(parentFrame);        
-        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);   
-        
-        this.setTitle(" Login Dialog. Bitte User und Password eingeben");  
-        
-	this.setType(Type.UTILITY);
-	this.setBounds(100, 100, 325, 210);
-	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	this.getContentPane().setLayout(null);
-	
-	JButton btnNewButton = new JButton("Cancel");
-	btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 12));
-	btnNewButton.setBounds(204, 112, btn_width, btn_height);
-	this.getContentPane().add(btnNewButton);
-	
-	JButton btnCa = new JButton("Login");
-	btnCa.setFont(new Font("Tahoma", Font.PLAIN, 12));
-	btnCa.setBounds(115, 112, btn_width, btn_height);
-	this.getContentPane().add(btnCa);
-	
-	JLabel lblUser = new JLabel("User:");
-	lblUser.setFont(new Font("Tahoma", Font.PLAIN, 14));
-	lblUser.setBounds(37, 34, lbl_width, lbl_height);
-	this.getContentPane().add(lblUser);
-	
-	JLabel lblPasswort = new JLabel("Passwort:");
-	lblPasswort.setToolTipText("Bitte Passwort eingeben und einloggen");
-	lblPasswort.setFont(new Font("Tahoma", Font.PLAIN, 14));
-	lblPasswort.setBounds(37, 69, lbl_width, lbl_height);
-	this.getContentPane().add(lblPasswort);
-	
-	tfUsername = new JTextField();
-	tfUsername.setBounds(115, 28, tf_width, tf_height);
-	
-	this.getContentPane().add(tfUsername);
-	
-	pfPassword = new JPasswordField();
-	pfPassword.setBounds(115, 66, tf_width, tf_height);
-	this.getContentPane().add(pfPassword);              
-                  
+    private void initializeComponents(Frame parent)
+    {
+        parentFrame = parent;
+        this.setBounds(100, 100, 300, 200);
 
-  }
+        this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        gridBagLayout.columnWidths = new int[]
+        { 30, 44, 57, 45, 65, 0 };
+        gridBagLayout.rowHeights = new int[]
+        { 59, 20, 20, 23, 0 };
+        gridBagLayout.columnWeights = new double[]
+        { 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+        gridBagLayout.rowWeights = new double[]
+        { 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+        this.getContentPane().setLayout(gridBagLayout);
+
+        JLabel lblUser = new JLabel("User");
+        GridBagConstraints gbc_lblUser = new GridBagConstraints();
+        gbc_lblUser.anchor = GridBagConstraints.EAST;
+        gbc_lblUser.insets = new Insets(0, 0, 5, 5);
+        gbc_lblUser.gridx = 2;
+        gbc_lblUser.gridy = 1;
+        this.getContentPane().add(lblUser, gbc_lblUser);
+
+        tfUsername = new JTextField();
+        GridBagConstraints gbc_textField = new GridBagConstraints();
+        gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+        gbc_textField.insets = new Insets(0, 0, 5, 5);
+        gbc_textField.gridx = 3;
+        gbc_textField.gridy = 1;
+        this.getContentPane().add(tfUsername, gbc_textField);
+        tfUsername.setColumns(10);
+
+        JButton btnCancel = new JButton("Cancel");
+
+        btnCancel.addActionListener(this);
+
+        JButton btnLogin = new JButton("Login");
+
+        btnLogin.addActionListener(this);
+
+        JLabel lblPw = new JLabel("Passwort");
+        GridBagConstraints gbc_lblPw = new GridBagConstraints();
+        gbc_lblPw.anchor = GridBagConstraints.EAST;
+        gbc_lblPw.insets = new Insets(0, 0, 5, 5);
+        gbc_lblPw.gridx = 2;
+        gbc_lblPw.gridy = 2;
+        this.getContentPane().add(lblPw, gbc_lblPw);
+
+        pfPassword = new JPasswordField();
+        GridBagConstraints gbc_passwordField = new GridBagConstraints();
+        gbc_passwordField.fill = GridBagConstraints.HORIZONTAL;
+        gbc_passwordField.insets = new Insets(0, 0, 5, 5);
+        gbc_passwordField.gridx = 3;
+        gbc_passwordField.gridy = 2;
+        this.getContentPane().add(pfPassword, gbc_passwordField);
+        GridBagConstraints gbc_btnCancel = new GridBagConstraints();
+        gbc_btnCancel.insets = new Insets(0, 0, 0, 5);
+        gbc_btnCancel.gridx = 3;
+        gbc_btnCancel.gridy = 3;
+        this.getContentPane().add(btnCancel, gbc_btnCancel);
+        GridBagConstraints gbc_button = new GridBagConstraints();
+        gbc_button.gridx = 4;
+        gbc_button.gridy = 3;
+        this.getContentPane().add(btnLogin, gbc_button);
+    }
 
     public loginDialog(Frame parent)
     {
@@ -99,23 +114,195 @@ public class loginDialog extends JDialog  implements  ActionListener
         //
         initializeComponents(parent);
     }
-  
 
-    public String getUsername() {
+    public String getUsername()
+    {
         return tfUsername.getText().trim();
     }
 
-    public String getPassword() {
+    public String getPassword()
+    {
         return new String(pfPassword.getPassword());
     }
 
-    public boolean isSucceeded() {
+    public boolean isSucceeded()
+    {
         return succeeded;
     }
+
     @Override
     public void actionPerformed(ActionEvent e)
     {
-	// TODO Auto-generated method stub
-	
+        Object obj = e.getSource();
+        if (debug)
+            System.out.println("actionPerformed event :" + e.getActionCommand());
+
+        if (obj instanceof JButton)
+        {
+            JButton btn = (JButton) obj;
+            if (debug)
+                System.out.println(" ActinCommand  : " + btn.getActionCommand() + btn.getName());
+            // + btnCancel.getActionCommand().toString() +
+            // btnLogin.getActionCommand().toString());
+
+            switch (btn.getActionCommand())
+            {
+            case "Login":
+                if (debug)
+                    System.out.println(" ActionCommand = " + "Login");
+
+                if (getUsername() == null || getPassword() == null)
+                {
+                    JOptionPane.showMessageDialog(loginDialog.this, "Bitte Passwort und Username eingeben", "Login", JOptionPane.ERROR_MESSAGE);
+                    break;
+                }
+                
+                
+                login mlog = new login(getUsername(), getPassword());
+
+                if (mlog.authenticate(getUsername(), getPassword()))
+                {
+                    System.out.println(" login.authenticate =  " + getUsername() + getPassword());
+
+                    if (debug)
+                    {
+                        JOptionPane.showMessageDialog(loginDialog.this, "Moin, Moin " + getUsername() + "! Sie sind erfolgreich eingeloggt.", "Login",
+                                JOptionPane.INFORMATION_MESSAGE);
+                    }
+
+                    succeeded = true;
+                    windowClosing(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+                }
+                else
+                {
+                    JOptionPane.showMessageDialog(loginDialog.this, "username oder passwort falsch", "Login", JOptionPane.ERROR_MESSAGE);
+                    // reset username and password
+                    tfUsername.setText("");
+                    pfPassword.setText("");
+                    succeeded = false;
+                }
+
+                break;
+            case "Cancel":
+                windowClosing(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+                break;
+            default:
+                break;
+            }
+        }
+    }
+
+    // if (btn.getActionCommand().equals("Login"))
+    // {
+    // // login
+    // System.out.println(" ActionCommand = " + "Login" );
+    // System.out.println("actionPerformed event :" + " LogIN");
+    // if (login.authenticate(getName(), getPassword())) {
+    // System.out.println("Erfolgreich eingeloggt " + obj.toString());
+    // JOptionPane.showMessageDialog(loginDialog.this,
+    // "Moin, Moin " + getUsername() + "! Sie sind erfolgreich eingeloggt.",
+    // "Login",
+    // JOptionPane.INFORMATION_MESSAGE);
+    // succeeded = true;
+    // // TODO Lüthke Erfolgsmeldung auf dem Titel des aufrufenden
+    // // frame schreiben
+    // dispose();
+    // } else {
+    // System.out.println("nicht eingeloggt " + obj.toString());
+    // JOptionPane.showMessageDialog(loginDialog.this, "falscher username der
+    // falsches password", "Login",
+    // JOptionPane.ERROR_MESSAGE);
+    // // reset username and password
+    // tfUsername.setText("");
+    // pfPassword.setText("");
+    // succeeded = false;
+    // }
+    //
+    //
+    // if (btn.getActionCommand().equals("Cancel"))
+    // System.out.println(" ActionCommand = " + "Cancel" );
+    //
+    //
+    // if ( btn.getActionCommand().equals("Cancel") )
+    // if (debug)
+    // {
+    // System.out.println("WindowClosing event setzen :" +
+    // e.getActionCommand());
+    // windowClosing(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+    // }
+    // else {
+    // // login
+    // System.out.println("actionPerformed event :" + " LogIN");
+    // if (login.authenticate(getName(), getPassword())) {
+    // System.out.println("Erfolgreich eingeloggt " + obj.toString());
+    // JOptionPane.showMessageDialog(loginDialog.this,
+    // "Moin, Moin " + getUsername() + "! Sie sind erfolgreich eingeloggt.",
+    // "Login",
+    // JOptionPane.INFORMATION_MESSAGE);
+    // succeeded = true;
+    // // TODO Lüthke Erfolgsmeldung auf dem Titel des aufrufenden
+    // // frame schreiben
+    // dispose();
+    // } else {
+    // System.out.println("nicht eingeloggt " + obj.toString());
+    // JOptionPane.showMessageDialog(loginDialog.this, "falscher username der
+    // falsches password", "Login",
+    // JOptionPane.ERROR_MESSAGE);
+    // // reset username and password
+    // tfUsername.setText("");
+    // pfPassword.setText("");
+    // succeeded = false;
+    // }
+    // }
+
+    @Override
+    public void windowActivated(WindowEvent arg0)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void windowClosed(WindowEvent arg0)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent arg0)
+    {
+
+        // Ruft die Methode windowClosed() auf
+        this.dispose();
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent arg0)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent arg0)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent arg0)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void windowOpened(WindowEvent arg0)
+    {
+        // TODO Auto-generated method stub
+
     }
 }
